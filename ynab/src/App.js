be3ecc2aeea4ce0d3a8013b8269b07eeedb440ec
Jamaal <jjrog16@gmail.com>
2021-12-01 20:57:19 -0500

    Init project

---
+++ ynab/src/App.js
@@ -0,0 +1,16 @@
+import "../src/styles/App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
