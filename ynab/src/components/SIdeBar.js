5a19a6dcf264e69b17f95e2b048a61dc9989bd0b
Jamaal <jjrog16@gmail.com>
2021-12-03 19:06:45 -0500

    Organized styles

--- ynab/src/components/SIdeBar.js
+++ ynab/src/components/SIdeBar.js
@@ -5,13 +5,22 @@ function SideBar() {
   return (
     <div className="sidebar-container">
       <div className="account-header">
-        <h1 className="current-budget">My Budget</h1>
+        <h2 className="current-budget">My Budget</h2>
         <div className="user-account">example@email.com</div>
       </div>
       <div className="overview-sections">
-        <h2 className="page-overview">Budget</h2>
+        <h3 className="page-overview">Budget</h3>
       </div>
-      <div className="all-accounts"></div>
+      <div className="all-accounts">
+        <div className="budget-header">
+          <div className="budget-title">BUDGET</div>
+          <div className="budget-total-amount">$1,000</div>
+        </div>
+        <div className="account">
+          <div className="account1-name">Fake Bank</div>
+          <div className="account1-amount">$1,000</div>
+        </div>
+      </div>
       <button className="add-account">Add account</button>
     </div>
   );
