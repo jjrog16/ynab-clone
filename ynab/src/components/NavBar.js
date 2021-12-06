383ecaceeb8637106e0a2906fb8310bec62c0f2b
Jamaal <jjrog16@gmail.com>
2021-12-06 18:49:12 -0500

    Renamed Sidebar.js from SIdeBar

--- ynab/src/components/NavBar.js
+++ ynab/src/components/NavBar.js
@@ -2,9 +2,14 @@ import React from "react";
 
 function NavBar() {
   return (
-    <div>
-      <div className="test">NavBar</div>
-    </div>
+    <nav className="navbar">
+      <div className="date">DEC 2021</div>
+      <div className="ready-to-assign">
+        <div className="ready-to-assign-amount">$1,000.00</div>
+        <p className="ready-to-assign-title">Ready to Assign</p>
+        <button className="btn-assign">Assign</button>
+      </div>
+    </nav>
   );
 }
 
