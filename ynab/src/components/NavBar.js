0753743515e7dc58d66256873c1beaee7aaf4594
Jamaal <jjrog16@gmail.com>
2021-12-06 19:25:22 -0500

    Added basic styles for navbar

--- ynab/src/components/NavBar.js
+++ ynab/src/components/NavBar.js
@@ -1,13 +1,20 @@
 import React from "react";
+import "../styles/css/NavBar.css";
 
 function NavBar() {
   return (
     <nav className="navbar">
       <div className="date">DEC 2021</div>
       <div className="ready-to-assign">
-        <div className="ready-to-assign-amount">$1,000.00</div>
-        <p className="ready-to-assign-title">Ready to Assign</p>
-        <button className="btn-assign">Assign</button>
+        <div className="ready-to-assign-left">
+          <div className="ready-to-assign-amount">$1,000.00</div>
+          <p className="ready-to-assign-title">Ready to Assign</p>
+        </div>
+        <div className="read-to-assign-right">
+          <div className="btn-holder">
+            <button className="btn-assign">Assign</button>
+          </div>
+        </div>
       </div>
     </nav>
   );
