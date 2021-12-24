d3b0be9d39cd19ff6e3356b5d933ea177461305b
Jamaal <jjrog16@gmail.com>
2021-12-24 16:40:35 -0500

    Total budget amount now reflects amount from all accounts

--- ynab/src/components/AccountItem.tsx
+++ ynab/src/components/AccountItem.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import "../styles/css/AccountItem.css";
 
 function AccountItem(props: { key: string; info: DocumentData }) {
-  console.log(props);
   return (
     <>
       <li key={props.key} className="account">
