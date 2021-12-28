094fb7bbbfc358230738ddc22e2fbf315d362af5
Jamaal <jjrog16@gmail.com>
2021-12-28 12:30:37 -0500

    Formatted money in sidebar

--- ynab/src/components/AccountItem.tsx
+++ ynab/src/components/AccountItem.tsx
@@ -3,11 +3,12 @@ import React from "react";
 import "../styles/css/AccountItem.css";
 
 function AccountItem(props: { id: string; info: DocumentData }) {
+  const fixedAmount = `$${Number(props.info.amount).toFixed(2)}`;
   return (
     <>
       <li key={props.id} className="account">
         <div className="account-name">{props.info.title}</div>
-        <div className="account-amount">{props.info.amount}</div>
+        <div className="account-amount">{fixedAmount}</div>
       </li>
     </>
   );
