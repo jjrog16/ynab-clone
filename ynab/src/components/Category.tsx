dcb5da89e523f55ef0f34c94d04f9c7e97cb05a2
Jamaal <jjrog16@gmail.com>
2021-12-29 16:42:15 -0500

    Adding and subtracting from available category balance works

--- ynab/src/components/Category.tsx
+++ ynab/src/components/Category.tsx
@@ -5,9 +5,9 @@ import {
   DocumentReference,
   getFirestore,
   QueryDocumentSnapshot,
+  setDoc,
 } from "@firebase/firestore";
-import { Query } from "@testing-library/react";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import "../styles/css/Category.css";
 import EditComponentPopup from "./EditComponentPopup";
 
@@ -73,8 +73,136 @@ function Category(props: Props) {
     setEditComponentPopupStatus(true);
   }
 
-  // Available money associated with a category
-  const [available, setAvailable] = useState("");
+  // Money user is attempting to add
+  const [inputState, setInputState] = useState<string>("");
+
+  // State for performing addition
+  const [isPlusActive, setIsPlusActive] = useState(false);
+
+  // State for performing subtraction
+  const [isMinusActive, setIsMinusActive] = useState(false);
+
+  // Status for loading API call
+  const [isSending, setIsSending] = useState(false);
+
+  // Keep track of when the component is unmounted
+  const isMounted = useRef(true);
+
+  // set isMounted to false when we unmount the component
+  useEffect(() => {
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  /**
+   * When isPlusActive is true, this function
+   * takes the input and updates the database
+   * asynchronously, rerenders the page, then
+   * updates the total Category Group amount
+   *
+   */
+  const addToAvailable = useCallback(async () => {
+    // Try to convert to number. If fails, exit
+    const strToNum: number = Number(inputState);
+
+    if (strToNum) {
+      // don't send again while we are sending
+      if (isSending) return;
+
+      // update state
+      setIsSending(true);
+
+      await setDoc(categoryDbLocation, {
+        available: props.category.data().available + strToNum,
+        groupId: props.category.data().groupId,
+        position: props.category.data().position,
+        title: props.category.data().title,
+      });
+
+      // once the request is sent, update state again
+      // only update if we are still mounted
+      if (isMounted.current) setIsSending(false);
+
+      // Load from Firebase to cause a rerender since there is a change
+      props.rerender();
+
+      // Update the total amount set for the categories so that Ready to Assign can
+      // update its state
+      props.setTotalCategoryGroupAmount(
+        (prevAmount: number) => prevAmount + strToNum
+      );
+
+      // Turn off checking for if Plus was clicked
+      setIsPlusActive(false);
+    }
+  }, [isSending, inputState]);
+
+  /**
+   * When isMinusActive is true, this function
+   * takes the input and updates the database
+   * asynchronously, rerenders the page, then
+   * updates the total Category Group amount
+   *
+   */
+  const subtractFromAvailable = useCallback(async () => {
+    // Try to convert to number. If fails, exit
+    const strToNum: number = Number(inputState);
+
+    if (strToNum) {
+      // don't send again while we are sending
+      if (isSending) return;
+
+      // update state
+      setIsSending(true);
+
+      await setDoc(categoryDbLocation, {
+        available: props.category.data().available - strToNum,
+        groupId: props.category.data().groupId,
+        position: props.category.data().position,
+        title: props.category.data().title,
+      });
+
+      // once the request is sent, update state again
+      // only update if we are still mounted
+      if (isMounted.current) setIsSending(false);
+
+      // Load from Firebase to cause a rerender since there is a change
+      props.rerender();
+
+      // Update the total amount set for the categories so that Ready to Assign can
+      // update its state
+      props.setTotalCategoryGroupAmount(
+        (prevAmount: number) => prevAmount - strToNum
+      );
+
+      // Turn off checking for if Minus was clicked
+      setIsMinusActive(false);
+    }
+  }, [isSending, inputState]);
+
+  /**
+   * Handles the user input selection and chooses the correct
+   * function to run based on if Plus or Minus was selected.
+   * @param e KeyBoardEvent the user enters in the input field
+   */
+  function handleKeyDown(e: React.KeyboardEvent) {
+    // Review event if key pressed is the enter key
+    if (e.key === "Enter") {
+      // Prevents keyDown from refreshing page
+      e.preventDefault();
+      if (isPlusActive) {
+        addToAvailable();
+      }
+      if (isMinusActive) {
+        subtractFromAvailable();
+      }
+    }
+  }
+
+  // System colors in SCSS
+  const LIGHT_BLUE = "#4795d8";
+  const DARK_GRAY = "#656568";
 
   return (
     <>
@@ -99,12 +227,35 @@ function Category(props: Props) {
           <div className="category-name">{category.title}</div>
         </div>
         <div className="category-right-side">
+          <div
+            className="plus calc-category-available"
+            style={{ color: isPlusActive ? LIGHT_BLUE : DARK_GRAY }}
+            onClick={() => {
+              setIsPlusActive(!isPlusActive);
+              // Set minus to false since you cannot do both
+              setIsMinusActive(false);
+            }}
+          >
+            +
+          </div>
+          <div
+            className="minus calc-category-available"
+            style={{ color: isMinusActive ? LIGHT_BLUE : DARK_GRAY }}
+            onClick={() => {
+              setIsMinusActive(!isMinusActive);
+              // Set plus to false since you cannot do both
+              setIsPlusActive(false);
+            }}
+          >
+            -
+          </div>
           <form>
             <input
               type="text"
               id="et-edit-available"
-              value={available}
-              onChange={(e) => setAvailable(e.target.value)}
+              value={inputState}
+              onChange={(e) => setInputState(e.target.value)}
+              onKeyDown={(e) => handleKeyDown(e)}
             ></input>
           </form>
           <div className="category-amount">{categoryAvailableFixed}</div>
