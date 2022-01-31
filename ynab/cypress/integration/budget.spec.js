e80afb75a6db36d477035eeb68c09e555ccec9b0
Jamaal <jjrog16@gmail.com>
2022-01-30 19:18:59 -0500

    Setting up plan for testing budget section

---
+++ ynab/cypress/integration/budget.spec.js
@@ -0,0 +1,14 @@
+describe("category group", () => {
+  it.skip("User can add a category group");
+  it.skip("User can edit category group name");
+  it.skip("User can delete category group");
+});
+describe("categories", () => {
+  it.skip("User can add a category group");
+  it.skip("User can add a category");
+  it.skip("User can edit a category name");
+  it.skip("User can add money to a category");
+  it.skip("User can subtract money from a category");
+  it.skip("User can delete category");
+  it.skip("User can delete category group");
+});
